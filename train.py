bbacba1d395cb8f72df7d5644a91923de4df0741
pkiran76 <71447314+pkiran76@users.noreply.github.com>
2021-03-22 23:20:27 +0530

    try QDA

--- train.py
+++ train.py
@@ -1,6 +1,26 @@
 import pandas as pd 
 import numpy as np
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn import preprocessing
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve
+from sklearn.model_selection import train_test_split
+import json
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer
+
+df = pd.read_csv("data_processed.csv")
+
+#### Get features ready to model! 
+y = df.pop("cons_general").to_numpy()
+y[y< 4] = 0
+y[y>= 4] = 1
+
+X = df.to_numpy()
+X = preprocessing.scale(X) # Is standard
+# Impute NaNs
 
 imp = SimpleImputer(missing_values=np.nan, strategy='mean')
 imp.fit(X)
@@ -31,3 +51,4 @@ sns.set_color_codes("dark")
 ax = sns.barplot(x="region", y="pred_accuracy", data=df, palette = "Greens_d")
 ax.set(xlabel="Region", ylabel = "Model accuracy")
 plt.savefig("by_region.png",dpi=80)
+
